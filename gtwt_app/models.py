435aaf5ae2fee1941e239633ca2245a8aae17523
nakulshah04 <shahnakul@gmail.com>
2025-04-15 17:08:36 -0400

    allowing admin to edit user reports

--- gtwt_app/models.py
+++ gtwt_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import admin
 
 class ConstructionZone(models.Model):
     description = models.TextField()
@@ -9,3 +10,9 @@ class ConstructionZone(models.Model):
 
     def __str__(self):
         return f"{self.description[:30]} ({self.start_date} to {self.end_date})"
+
+@admin.register(ConstructionZone)
+class ConstructionZoneAdmin(admin.ModelAdmin):
+    list_display = ("description", "start_date", "end_date", "created_at")
+    search_fields = ("description",)
+    list_filter = ("start_date", "end_date")
