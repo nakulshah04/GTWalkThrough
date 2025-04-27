08fdc39e9e94d9d0a87ee433b106527b55982b4a
Lucia Jang <luciajang0314@gmail.com>
2025-04-27 04:10:52 -0400

    Fix email field on registration page

--- gtwt_app/views.py
+++ gtwt_app/views.py
@@ -46,7 +46,7 @@ def about(request):
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2']
 
 # Register View
 def register_view(request):
